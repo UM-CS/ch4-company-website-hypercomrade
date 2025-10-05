2cc4bec91d86853f0a3ccc2400cd4c69856dfecd
Mikhail <mckaylbing@gmail.com>
2025-10-05 15:21:06 -0600

    Add more pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
+# pages/views.py
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+def home_page_view(request):
+    return render(request, "home.html")
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
